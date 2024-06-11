369a7033b61901156d116ec66f1f13d2c5488570
EmanueleGurini <emanuele.gurini89@gmail.com>
2024-06-11 20:50:13 +0200

    Feat: add style on route change

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,32 @@
-import styles from './navbar.module.css';
-import Logo from '../Logo/Logo';
+// src/components/NavBar/NavBar.jsx
+
+import styles from "./navbar.module.css";
+import Logo from "../Logo/Logo";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  const menuList = ['Home', 'About', 'Contacts'];
+  //const menuList = ["Home", "About", "Contacts"];
+
+  const menuList = [
+    // New Line
+    { name: "Home", path: "/" }, // New Line
+    { name: "About", path: "/about" }, // New Line
+    { name: "Contacts", path: "/contacts" }, // New Line
+  ];
 
   return (
     <nav className={styles.navbarContainer}>
       <Logo />
       <ul className={styles.menuList}>
         {menuList.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            <NavLink
+              to={item.path}
+              className={({ isActive }) => (isActive ? styles.active : "")}
+            >
+              {item.name}
+            </NavLink>{" "}
+          </li>
         ))}
       </ul>
     </nav>
